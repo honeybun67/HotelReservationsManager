49f9e14d592317c2bde5e7710dd007748d6e00dd
Atanascho <atanasmi06@abv.bg>
2025-04-02 17:00:48 +0300

    CreateReservationViewModel

--- HotelReservationsManager/ViewModels/Reservations/CreateReservationViewModel.cs
+++ HotelReservationsManager/ViewModels/Reservations/CreateReservationViewModel.cs
@@ -7,6 +7,27 @@ namespace HotelReservationsManager.ViewModels.Reservations
 {
     public class CreateReservationViewModel
     {
-        
+        public string UserId { get; set; }
+        public int RoomCapacity { get; set; }
+
+        [Required(ErrorMessage = "Please select and submit a room")]
+        public string RoomId { get; set; }
+        public SelectList? Rooms { get; set; }
+        public virtual ICollection<Client> Clients { get; set; } = new HashSet<Client>();
+
+        [Required]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        [DataType(DataType.Date)]
+        [DisplayName("Accomodation date")]
+        public DateTime AccommodationDate { get; set; }
+        [Required]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        [DataType(DataType.Date)]
+        [DisplayName("Accomodation date")]
+        public DateTime LeaveDate { get; set; }
+        [DisplayName("Breakfast")]
+        public bool WithBreakfast { get; set; }
+        [DisplayName("Allinclusive")]
+        public bool AllInclusive { get; set; }
     }
 }
